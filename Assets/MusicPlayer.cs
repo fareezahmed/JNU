682e6a7fc8021f611da7e3b87b8d42e2b6a3c100
Fareez <fareez1234@gmail.com>
2020-03-31 13:02:36 +1100

    JNU - Argon Assault - Lesson 97

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MusicPlayer : MonoBehaviour
+{
+    private void Awake() {
+        DontDestroyOnLoad(gameObject);
+    }
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Invoke("LoadFirstLevel", 2f);
+    }
+
+    private void LoadFirstLevel() {
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    [Tooltip("In m^-1")][SerializeField] float xSpeed = 55f;
+    [Tooltip("In m^-1")][SerializeField] float xRange = 25f;
+
+    [Tooltip("In m^-1")][SerializeField] float ySpeed = 55f;
+    [Tooltip("In m^-1")][SerializeField] float yRange = 15f;
+
+    [SerializeField] float positionPitchFactor = -0.15f;
+    [SerializeField] float controlPitchFactor = -2f;
+
+    [SerializeField] float positionYawFactor = -2f;
+    [SerializeField] float controlRollFactor = -17f;
+    float xThrow, yThrow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessTranslation();
+        ProcessRotation();
+    }
+
+    private void ProcessRotation() {
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToControl = yThrow * controlPitchFactor;
+        float pitch = pitchDueToPosition + pitchDueToControl;
+        
+        float yaw = transform.localPosition.x * positionYawFactor;
+        
+        float roll = xThrow * controlRollFactor;
+        
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    private void ProcessTranslation() {
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = xThrow * xSpeed * Time.deltaTime;
+        float rawXPos  = transform.localPosition.x + xOffset;
+        float clampXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float yOffset = yThrow * ySpeed * Time.deltaTime;
+        float rawYPos  = transform.localPosition.y + yOffset;
+        float clampYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
+
+        transform.localPosition = new Vector3(clampXPos, clampYPos, transform.localPosition.z);
+    }
+}
